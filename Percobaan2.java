7af59bb19be797f6647ce1f351dacbb26d6aba13
Deizan <deizanfakhri7@gmail.com>
2025-12-23 23:15:45 +0700

    percobaan 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 
 public class Percobaan2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Bilangan yang dihitung: ");
