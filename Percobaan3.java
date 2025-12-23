22077b1cb3f3216626ca5617db8efcc0e9ddf96d
Deizan <deizanfakhri7@gmail.com>
2025-12-23 23:16:10 +0700

    percobaan 3

--- Percobaan3.java
+++ Percobaan3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Percobaan3 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
     Scanner sc = new Scanner(System.in);
     
     System.out.print("Jumlah Saldo Awal : ");
